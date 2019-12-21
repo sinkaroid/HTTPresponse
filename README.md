82ea692fda6a409b42127f2d3822406ee7c70816
sinkaroid <bibsindra@gmail.com>
2019-12-21 22:26:47 +0700

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# HTTPresponse  
+mass http response // also can use to check many webshells  
+
+[![CodeFactor](https://www.codefactor.io/repository/github/sinkaroid/httpresponse/badge)](https://www.codefactor.io/repository/github/sinkaroid/httpresponse)  
+
+----
+     $ ./http.sh  
+
+![](https://i.imgur.com/99yYf1N.png)
---
+++ http.sh
@@ -0,0 +1,23 @@
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1) 
+CYAN=$(tput setaf 6) 
+
+echo -e "${RED}
+
+  _    _ _______ _______ _____                                          
+ | |  | |__   __|__   __|  __ \                                         
+ | |__| |  | |     | |  | |__) | __ ___  ___ _ __   ___  _ __  ___  ___ 
+ |  __  |  | |     | |  |  ___/ '__/ _ \/ __| '_ \ / _ \| '_ \/ __|/ _ |
+ | |  | |  | |     | |  | |   | | |  __/\__ \ |_) | (_) | | | \__ \  __/
+ |_|  |_|  |_|     |_|  |_|   |_|  \___||___/ .__/ \___/|_| |_|___/\___|
+                                            | |                         
+                                            |_|mass http checker // webshells checker"
+read -p "${WHITE}raw list: " raw 
+echo -e "\n"
+read -p "${WHITE}${raw} Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+#do here
+wget -q -nv -O bajing.txt ${raw};
+cat bajing.txt | sed 's/htt/curl -LI htt/' | sed "s/$/ -o \/dev\/null -w '${CYAN}%{http_code} => '${GREEN}'%{url_effective}${RED}\n' -s/" > mek.sh
+
+./mek.sh;rm bajing.txt
